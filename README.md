5fe22bb482bb397916172e004a5d536cc76b7e2b
maumatus <86168992+maumatus@users.noreply.github.com>
2022-02-16 23:52:02 -0300

    segundo commit-funciona scrapping

---
+++ README.md
@@ -0,0 +1,7 @@
+Web scrapping app
+
+forked from ??
+
+Web-Scrapper V01 GET from node Backend
+Web-Scrapper V02 Input web direction from HTML Input with URL validation as security.
+
--- app.js
+++ app.js
@@ -3,7 +3,32 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 //Definimos la url desde la que se hara scrapping. 
 //Luego se agregara un input desde DOM y una interfaz.
-const url = ''
+
+const url = 'https://dragonframe.com/'
+
+
+axios(url)
+
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        //console.log(html)
+        const articles = []
+
+        $('a', html).each(function() { //<-- cannot be a function expression
+            const title = $(this).text()
+            //const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+        })
+        //$.html();
+        console.log(articles)
+    }).catch(err => console.log(err))
+
+app.listen(PORT, () => console.log(`server running on http://localhost:${PORT}`));
